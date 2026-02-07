c9ebb2a4aae92f665d84596e195dd857d3751366
2025aa05426-wq <2025aa05426@wilp.bits-pilani.ac.in>
2026-02-08 00:31:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,23 @@
 
+import streamlit as st
+import pandas as pd
+import joblib
+
+st.title("Mental Health Care Options Classifications")
+
+uploaded_file = st.file_uploader("Upload your data (CSV)", type="csv")
+
+model_name = st.selectbox(
+    "Select Model",
+    ["Logistic Regression", "Decision Tree", "KNN",
+     "Naive Bayes", "Random Forest", "XGBoost"]
+)
+
+if uploaded_file:
+    data = pd.read_csv(uploaded_file)
+    st.write(data.head())
+
+    model = joblib.load(f"model/{model_name}.pkl")
+    predictions = model.predict(data)
+
+    st.write("Predictions:", predictions)
